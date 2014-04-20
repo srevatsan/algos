c181a496cfa232e3995671491c3b86f3b0bb00f6
Sri Muralidharan <srevatsan@edmodo.com>
2014-04-19 20:15:21 -0700

    stack class creation

--- stack.py
+++ stack.py
@@ -1,2 +1,26 @@
 __author__ = 'smuralid'
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+x = Stack()
+x.push(1)
+x.push('ad')
+x.pop()
+x.pop()
